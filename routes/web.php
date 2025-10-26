2c2194022f892c381ae436a7bf80f242c751bf60
Grzesiek <gblazusiak@gmail.com>
2025-10-26 23:58:13 +0100

    fix: Restore correct route order for posts/create

--- routes/web.php
+++ routes/web.php
@@ -25,13 +25,24 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\Admin\UserController;
 
-// Public routes
+// Posts routes - specific routes BEFORE wildcard routes
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+
+Route::middleware('auth')->group(function () {
+    // Posts routes - CREATE must be before SHOW to avoid route collision
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+});
+
+// Public post show - after /posts/create
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
 // Authenticated routes
 Route::middleware('auth')->group(function () {
-    Route::resource('posts', PostController::class)->except(['index', 'show']);
+    // Posts edit/update/delete
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
     
     // Comments routes
     Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
